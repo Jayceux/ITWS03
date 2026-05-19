422b9c95f18023ab2e5aa59207ebecbfe0c0482f
Jc Luy <155753565+Jayceux@users.noreply.github.com>
2026-05-19 17:20:09 +0800

    Add Database class for handling database connections and queries

---
+++ Framework/Database.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Framework;
+
+use PDO;
+use PDOException;
+use PDOStatement;
+use Exception;
+
+class Database
+{
+	public $conn;
+
+	/**
+	 * Database constructor.
+	 *
+	 * @param array $config
+	 */
+	public function __construct($config)
+	{
+		$dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+
+		$options = [
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+		];
+
+		try {
+			$this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+		} catch (PDOException $e) {
+			throw new Exception("Database connection failed: " . $e->getMessage());
+		}
+	}
+
+	/**
+	 * Execute a SQL query and return the results.
+	 *
+	 * @param string $query
+	 * @return PDOStatement
+	 * @throws Exception
+	 */
+	public function query($query, $params = [])
+	{
+		try {
+			$sth = $this->conn->prepare($query);
+
+			// Bind named parameters if provided
+			foreach ($params as $key => $value) {
+				$sth->bindValue(":" . $key, $value);
+			}
+
+			$sth->execute();
+			return $sth;
+		} catch (PDOException $e) {
+			throw new Exception("Query failed: " . $e->getMessage());
+		}
+	}
+}
